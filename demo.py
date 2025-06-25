d0e95980441704c1bb2650a09fc5a18285ebb626
saygus-alip <tchamnandong2@gmail.com>
2025-06-25 16:03:56 +0700

    Delete Code Editor
    delete

--- demo.py
+++
@@ -1,9 +0,0 @@
-def demo():
-    print("This is a demo function.")
-    print("It doesn't do much, but it's here to show how to use the module.")
-    print("You can call this function to see a demonstration of the module's functionality.")
-    print("Feel free to modify it as needed!")
-
-if __name__ == "__main__":
-    demo()
-    print("This module is being run directly.")
